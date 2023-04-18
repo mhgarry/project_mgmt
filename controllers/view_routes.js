dfe9e427605af83db9fd53a41753d5169c46faaa
MatthewM90 <matthewmoraga90@gmail.com>
2023-04-18 15:39:00 -0400

    added routes

---
+++ controllers/view_routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const handlebar = require("express-handlebars")
+const path = require("path");
+
+router.get("/", async (req, res) => {
+    res.render("index", {
+        
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const view_routes = require("./controllers/view_routes");
+const { engine } = require("express-handlebars");
+
+
 // add api/hbc routes
-const handlebars = require('handlebars');
 
 const app = express();
 
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.engine("hbs", engine({ 
+   extname: ".hbs" 
+}));
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use("/", view_routes);
 
 // app.engine('hbs', engine({
 //   extname: '.hbs' // change the file extension from .handlebars to .hbs
