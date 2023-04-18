03dd8de9c035c9bbcd4bf622f78f432d24d20410
Matt <mhgarry92@gmail.com>
2023-04-18 14:44:00 -0400

    start server

--- server.js
+++ server.js
@@ -5,4 +5,15 @@ const handlebars = require('handlebars');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+// app.engine('hbs', engine({
+//   extname: '.hbs' // change the file extension from .handlebars to .hbs
+// }));
+
+
+// app.set('view engine', 'hbs');
+// app.set('/'); // add routes in here when done
+
+app.listen(PORT, () => console.log('Server started on port %s', PORT));
