29a60d930f18cb05e61a2b254eb607b71c1dbbcd
Matt <mhgarry92@gmail.com>
2023-04-18 14:32:45 -0400

    installed npm package axios

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 // add api/hbc routes
+const handlebars = require('handlebars');
+
+const app = express();
+
+app.use(express.static('public'));
