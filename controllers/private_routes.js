ff02a9efc098b02d073db25ac1372f38b47465da
MatthewM90 <matthewmoraga90@gmail.com>
2023-04-22 12:16:04 -0400

    added function to the cancel buttons, and sign up/log in buttons

--- controllers/private_routes.js
+++ controllers/private_routes.js
@@ -4,7 +4,7 @@ const Card = require("../models/Card")
 
 function isAuthenticated(req, res, next) {
     if (!req.session.user_id) {
-        return res.redirect("/login");
+        return res.redirect("/");
     }
     next();
 }
@@ -14,6 +14,13 @@ router.get("/dashboard", isAuthenticated, async (req, res) => {
     res.render("dashboard", {
         email: user.email
     });
+});
+
+router.get("/projects", isAuthenticated, async (req, res) => {
+  const user = await User.findByPk(req.session.user_id);
+  res.render("projects", {
+      email: user.email
+  });
 });
 
 router.post("/cards", async (req, res) => {
