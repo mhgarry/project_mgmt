f40caba45eba19cd2030288c56d371eeb0a285cb
scelsic2 <scelsic2@gmail.com>
2023-04-23 11:28:49 -0400

    card-id page route

--- public/js/post.js
+++ public/js/post.js
@@ -4,20 +4,24 @@ const postCard = async function (event) {
    const task_title = document.querySelector(".title-input");
    const task_desc = document.querySelector(".task-descrip");
    const task_cat = document.querySelector(".cat-input");
-
+   const teammate_id = document.querySelector
+   ("#userDropdown > a")
+   
     await fetch ("/cards", {
         method: "POST",
         body: JSON.stringify({
             'task_title': task_title.value,
             'task_desc': task_desc.value,
-            'task_cat': task_cat.value
+            'task_cat': task_cat.value,
+            'teammate_id': teammate_id.value
         }),
         headers: { "Content-Type": "application/json"}
     })
 
     task_title.value = '';
     task_desc.value = "";
-    task_cat.value = ""
+    task_cat.value = "";
+    teammate_id.value = ""
 
 }
 
